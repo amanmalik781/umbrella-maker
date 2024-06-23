aa29f9ad9e2d7deaf07765568d8d60e666b1e8e4
AMAN DEEP MALIK <36561468+amanmalik781@users.noreply.github.com>
2024-06-23 14:54:26 +0530

    loader animation added

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const colorSwatches = document.querySelectorAll('.color-swatch');
     const umbrellaImage = document.getElementById('umbrella-image');
     const logoUpload = document.getElementById('logo-upload');
-    // const logoImage = document.getElementById('logo-image');
+    const logoImage = document.getElementById('logo-image');
     const loaderImage = document.getElementById('loader-image');
+    const loaderImageSmall = document.getElementById('loader-image-small');
     const uploadButton = document.getElementById('upload-button');
+    const uploadIcon = document.getElementById('upload-icon');
+    const deleteLogoIcon = document.getElementById('delete-logo-icon');
+    const uploadButtonText = document.getElementById('upload-button-text');
 
+    let loaderColor = 'blue';
     const backgroundColors = {
         blue: 'rgb(230, 246, 252)',
         yellow: 'rgb(255, 250, 237)',
@@ -20,22 +25,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     colorSwatches.forEach(swatch => {
         swatch.addEventListener('click', function () {
-            const color = swatch.getAttribute('data-color');
+            loaderColor = swatch.getAttribute('data-color');
             loaderImage.style.display = 'initial';
-            loaderImage.style.fill = color;
             umbrellaImage.style.display = 'none';
-            // logoImage.style.display = 'none';
-            showLoader();
-            document.body.style.backgroundColor = backgroundColors?.[color];
-            uploadButton.style.backgroundColor = buttonColors?.[color];
+            if (logoImage) {
+                logoImage.style.display = 'none';
+            }
+            showLoader(buttonColors?.[loaderColor]);
+            document.body.style.backgroundColor = backgroundColors?.[loaderColor];
+            uploadButton.style.backgroundColor = buttonColors?.[loaderColor];
             setTimeout(() => {
-                umbrellaImage.src = `assets/${color}_umbrella.png`;
+                umbrellaImage.src = `assets/${loaderColor}_umbrella.png`;
                 umbrellaImage.style.display = 'initial';
                 loaderImage.style.display = 'none';
                 hideLoader();
-                // if (logoImage.src) {
-                //     logoImage.style.display = 'block';
-                // }
+                if (logoImage?.src?.length > 32) {
+                    logoImage.style.display = 'initial';
+                }
             }, 1000);
         });
     });
@@ -48,15 +54,32 @@ document.addEventListener('DOMContentLoaded', function () {
         const file = event.target.files[0];
         if (file && (file.type === 'image/png' || file.type === 'image/jpeg')) {
             const reader = new FileReader();
-            loaderImage.style.display = 'block';
+            const fileName = logoUpload.files[0].name;
+            loaderImage.style.display = 'initial';
+            if (loaderImageSmall) {
+                loaderImageSmall.style.display = 'initial';
+            }
             umbrellaImage.style.display = 'none';
-            // logoImage.style.display = 'none';
+            uploadIcon.style.display = 'none';
+            showLoader(buttonColors?.[loaderColor]);
+            if (logoImage) {
+                logoImage.style.display = 'none';
+            }
             reader.onload = function (e) {
                 setTimeout(() => {
-                    // logoImage.src = e.target.result;
-                    // logoImage.style.display = 'block';
+                    if (logoImage) {
+                        logoImage.src = e.target.result;
+                        logoImage.style.display = 'initial';
+                    }
                     umbrellaImage.style.display = 'block';
                     loaderImage.style.display = 'none';
+                    if (loaderImageSmall) {
+                        loaderImageSmall.style.display = 'none';
+                    }
+                    uploadIcon.style.display = 'initial';
+                    deleteLogoIcon.style.visibility = 'visible';
+                    uploadButtonText.textContent = fileName;
+                    hideLoader();
                 }, 5000);
             };
             reader.readAsDataURL(file);
@@ -65,11 +88,30 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    function showLoader() {
-        loaderImage.classList.add('rotate');
+    deleteLogoIcon.addEventListener('click', (event) => {
+        event.stopPropagation(); // Stop event bubbling
+        logoImage.src = '';
+        logoImage.style.display = 'none';
+        uploadButtonText.textContent = 'UPLOAD LOGO';
+        deleteLogoIcon.style.visibility = 'hidden';
+        logoUpload.value = '';
+    });
+
+    function showLoader(color = buttonColors?.[loaderColor]) {
+        fetch('assets/loader_icon.svg') // Path to loader SVG file
+            .then(response => response.text())
+            .then(svg => {
+                const coloredSvg = svg.replace(/currentColor/g, color);
+                const svgBlob = new Blob([coloredSvg], { type: 'image/svg+xml' });
+                const svgUrl = URL.createObjectURL(svgBlob);
+                loaderImage.src = svgUrl;
+                loaderImage.classList.add('rotate');
+                loaderImageSmall.classList.add('rotate');
+            });
     }
 
     function hideLoader() {
         loaderImage.classList.remove('rotate');
+        loaderImageSmall.classList.add('rotate');
     }
 });
